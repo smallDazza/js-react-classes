9b9969c3555cca5c46eb7fc9921df3f953cc9753
smallDazza <smalldazza@gmail.com>
2024-11-16 10:02:55 +1100

    Feat: example of a dog class

---
+++ jsclasses/app.js
@@ -0,0 +1,14 @@
+class Dog {
+    constructor(name, breed, age, color){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+
+    
+}
+
+let millie = new Dog("Millie", "Spoodle", "3", "Grey/White");
+
+console.log(millie);
