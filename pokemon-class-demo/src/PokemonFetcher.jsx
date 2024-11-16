e88d4c911bdff43dcaf5af9978d423a0c6fb37d9
smallDazza <smalldazza@gmail.com>
2024-11-16 13:10:34 +1100

    Feat: added the new components in PokenmonCard and the key value

--- pokemon-class-demo/src/PokemonFetcher.jsx
+++ pokemon-class-demo/src/PokemonFetcher.jsx
@@ -8,18 +8,38 @@ export default class PokemonFetcher extends Component {
         super(props);
 
         this.state = {
-            pokemonList: []
+            pokemonList: [],
+            jwtToken: null
         }
     }
 
     async componentDidMount(){
-        // Generate a random Pokemon ID Number
-        let randomNumber = Math.ceil(Math.random() * 1025);
-        // Pass random Pokemon Number to fetch
-        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
-        let data = await response.json();
-        // set fetch response data to state
-        this.setState({pokemonList: [data.name]});
+        for (let index = 0; index < 6; index++) { 
+            // Generate a random Pokemon ID Number
+            let randomNumber = Math.ceil(Math.random() * 1025);
+            // Pass random Pokemon Number to fetch
+            let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
+            let data = await response.json();
+            // set fetch response data to state
+            // Method 1:
+            // this.setState({pokemonList: [...this.state.pokemonList, data.name]});
+            //Method 2:
+            this.setState((previousState) => {
+                return {
+                    pokemonList: [...previousState.pokemonList, data.name]
+                }              
+            })
+        }
+    }
+
+    componentDidUpdate(){
+        console.log("Page rendered. This is the current state: ", this.state.pokemonList);
+
+        if (this.state.jwtToken) {
+            console.log("User is logged in.");
+        } else {
+            console.log("User is logged out.");
+        }
     }
 
     render(){
@@ -27,8 +47,8 @@ export default class PokemonFetcher extends Component {
           <div>
             <h1>Pokemon Data</h1>
             {
-                this.state.pokemonList.map((pokemon) => {
-                    return <PokemonCard name={pokemon} />;
+                this.state.pokemonList.map((pokemon, index) => {
+                    return <PokemonCard key={pokemon + index} name={pokemon} />;
             })
             }
             <button onClick={() => {
